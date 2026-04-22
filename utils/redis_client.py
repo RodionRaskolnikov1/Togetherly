bbee8595045b7aa0ea4fa860c802e7cdb71aefa3
Het Raval <hetraval3333@gmail.com>
2026-04-22 12:16:02 +0530

    Fixed: redis_client

--- utils/redis_client.py
+++ utils/redis_client.py
@@ -7,7 +7,21 @@ load_dotenv()
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
 
-r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
-
-
+if REDIS_PASSWORD:
+    r = redis.Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        db=REDIS_DB,
+        password=REDIS_PASSWORD,
+        ssl=True,
+        decode_responses=True
+    )
+else:
+    r = redis.Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        db=REDIS_DB,
+        decode_responses=True
+    )
